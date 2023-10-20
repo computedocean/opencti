33ea838b8e908f288b242b9fed7dc9f99a9782e9
Cathia Archidoit <cathia.archidoit@filigran.io>
2023-10-20 11:46:58 +0200

    [backend] middleware.js: filters format (#2686)

--- opencti-platform/opencti-graphql/src/database/middleware-loader.ts
+++ opencti-platform/opencti-graphql/src/database/middleware-loader.ts
@@ -442,7 +442,9 @@ export const listEntities: InternalListEntities = async (context, user, entityTy
   const { indices = READ_ENTITIES_INDICES } = args;
   const { filters } = args;
   const convertedFilters = (noFiltersChecking || !filters) ? filters : checkedAndConvertedFilters(filters, entityTypes);
-  console.log('convertedFilters', convertedFilters);
+  if (!noFiltersChecking) {
+    console.log('convertedFilters', convertedFilters);
+  }
   // TODO Reactivate this test after global migration to typescript
   // if (connectionFormat !== false) {
   //   throw UnsupportedError('List connection require connectionFormat option to false');
